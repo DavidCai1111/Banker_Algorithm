a3bfb1b74a53c940c9e2681d3c4b9a6e86cd6417
davidcai <376462191@qq.com>
2014-12-15 23:14:16 +0800

    inprove views

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -28,4 +28,27 @@ $(function(){
         });
         $("tbody").append('<tr><td class="name">P'+lenNow+'</td><td><div class="row"><div class="col-xs-4"><input type="text" name="['+lenNow+'][0][0]" value="0" class="form-control"></div><div class="col-xs-4"><input type="text" name="['+lenNow+'][0][1]" value="0" class="form-control"></div><div class="col-xs-4"><input type="text" name="['+lenNow+'][0][2]" value="0" class="form-control"></div></div></td><td><div class="row"><div class="col-xs-4"><input type="text" name="['+lenNow+'][1][0]" value="0" class="form-control"></div><div class="col-xs-4"><input type="text" name="['+lenNow+'][1][1]" value="0" class="form-control"></div><div class="col-xs-4"><input type="text" name="['+lenNow+'][1][2]" value="0" class="form-control"></div></div></td><td><div class="row"><div class="col-xs-4"><input type="text" name="['+lenNow+'][2][0]" value="0" class="form-control"></div><div class="col-xs-4"><input type="text" name="['+lenNow+'][2][1]" value="0" class="form-control"></div><div class="col-xs-4"><input type="text" name="['+lenNow+'][2][2]" value="0" class="form-control"></div></div></td><td><button disabled="disabled" class="btn-danger btn">false</button></td><td><button class="btn-danger btn del">delete</button></td></tr>');
     });
+
+    //检查输入的数符合要求
+    var valueBefore;
+    $("input").focus(function(e){
+        var input = $(e.target);
+        valueBefore = input.val();
+    });
+    $("input").blur(function(e){
+        var input = $(e.target);
+        var valueAfter = input.val();
+        var patten=new RegExp(/^[0-9]+$/);
+        if(patten.test(valueAfter) !== true ||valueAfter < 0 || valueAfter > 99 || valueAfter.length > 2 ){
+          alert("输入的数需要是一个小于100的两位正整数！") ;
+          input.attr("value",valueBefore);
+          input.val(valueBefore);
+          return;
+        };
+        if(valueAfter[0] == 0 ){
+          valueAfter = valueAfter.slice(1,2);
+          input.attr("value",valueAfter);
+          input.val(valueAfter);
+        };
+    });
 });
